f9fe2af8e25167a49d103e3f408c30f915055529
Sylquivia <sylquivia@gmail.com>
2024-03-30 13:51:34 +0000

    added the blocks register

--- src/main/java/io/github/sylquivia/astrovia/Astrovia.java
+++ src/main/java/io/github/sylquivia/astrovia/Astrovia.java
@@ -1,19 +1,22 @@
 package io.github.sylquivia.astrovia;
 
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static io.github.sylquivia.astrovia.AstroviaBlocks.PLEASE_WORK;
+
 public class Astrovia implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Astrovia");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
 		AstroviaItems.register(mod);
+		AstroviaBlocks.register(mod);
 	}
 }
