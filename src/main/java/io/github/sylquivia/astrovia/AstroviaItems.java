89f9ecafda23efe0b0f3bff316d841ea1edf36f6
Sylquivia <sylquivia@gmail.com>
2024-03-30 13:52:18 +0000

    added block item for the block

--- src/main/java/io/github/sylquivia/astrovia/AstroviaItems.java
+++ src/main/java/io/github/sylquivia/astrovia/AstroviaItems.java
@@ -1,6 +1,7 @@
 package io.github.sylquivia.astrovia;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -9,14 +10,19 @@ import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
+import static io.github.sylquivia.astrovia.AstroviaBlocks.PLEASE_WORK;
+
 public class AstroviaItems {
 	public static final Item DANKIE_POO = new Item(new QuiltItemSettings());
 
 	public static void register(ModContainer mod) {
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "dankie_poo"), DANKIE_POO);
 
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "please_work"), new BlockItem(PLEASE_WORK, new QuiltItemSettings()));
+
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
 			entries.addItem(DANKIE_POO);
+			entries.addItem(PLEASE_WORK.asItem());
 		});
 	}
 }
