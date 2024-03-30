4ae1cdded0537442f109aa3d509215cd362305c3
Sylquivia <sylquivia@gmail.com>
2024-03-30 20:55:52 +0000

    did the blocks better so it blah blahs and .registers with the items

--- src/main/java/io/github/sylquivia/astrovia/Astrovia.java
+++ src/main/java/io/github/sylquivia/astrovia/Astrovia.java
@@ -1,14 +1,9 @@
 package io.github.sylquivia.astrovia;
 
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static io.github.sylquivia.astrovia.AstroviaBlocks.PLEASE_WORK;
 
 public class Astrovia implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Astrovia");
