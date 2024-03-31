db3a395898618a281af71e141ca80e515cde411c
Sylquivia <sylquivia@gmail.com>
2024-03-31 16:37:36 +0100

    WE HAVE TRANSPARENCY WOOOOO (added client class (for future, in quilt.mod.json u need to put client_init not just client))

---
+++ src/main/java/io/github/sylquivia/astrovia/AstroviaClient.java
@@ -0,0 +1,13 @@
+package io.github.sylquivia.astrovia;
+
+import net.minecraft.client.render.RenderLayer;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
+
+public class AstroviaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+		BlockRenderLayerMap.put(RenderLayer.getTranslucent(), AstroviaBlocks.FRACTIONATING_COLUMN);
+	}
+}
