dc37c3601ed7958884469281867e1410bb5d9ea5
Sylquivia <sylquivia@gmail.com>
2024-03-31 16:37:06 +0100

    WE HAVE TRANSPARENCY WOOOOO (made it a glass block)

--- src/main/java/io/github/sylquivia/astrovia/AstroviaBlocks.java
+++ src/main/java/io/github/sylquivia/astrovia/AstroviaBlocks.java
@@ -2,6 +2,7 @@ package io.github.sylquivia.astrovia;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.GlassBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -9,7 +10,7 @@ import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
 public class AstroviaBlocks {
-	public static final Block FRACTIONATING_COLUMN = new Block(QuiltBlockSettings.copyOf(Blocks.AMETHYST_BLOCK));
+	public static final Block FRACTIONATING_COLUMN = new GlassBlock((QuiltBlockSettings.copyOf(Blocks.AMETHYST_BLOCK).nonOpaque()));
 	public static void register(ModContainer mod) {
 		Registry.register(Registries.BLOCK, new Identifier(mod.metadata().id(), "fractionating_column"), FRACTIONATING_COLUMN);
 	}
