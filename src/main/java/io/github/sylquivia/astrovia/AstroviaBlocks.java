b22a77b361a21e5edccf9b6a2c206cecd82835dc
Sylquivia <sylquivia@gmail.com>
2024-04-02 22:15:24 +0100

    fixed it again ffs, believe it or not furni arent pipes and cannot extend

--- src/main/java/io/github/sylquivia/astrovia/AstroviaBlocks.java
+++ src/main/java/io/github/sylquivia/astrovia/AstroviaBlocks.java
@@ -12,7 +12,7 @@ import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 public class AstroviaBlocks {
 	public static final Block FRACTIONATING_COLUMN = new GlassBlock((QuiltBlockSettings.copyOf(Blocks.AMETHYST_BLOCK).nonOpaque()));
 	public static final Block HORIZONTAL_PIPE = new HorizontalPipe(QuiltBlockSettings.copyOf(Blocks.GLASS).nonOpaque());
-	public static final Block DIRECTIONAL_PIPE = new HorizontalPipe(QuiltBlockSettings.copyOf(Blocks.IRON_BLOCK).nonOpaque());
+	public static final Block DIRECTIONAL_PIPE = new DirectionalPipe(QuiltBlockSettings.copyOf(Blocks.IRON_BLOCK).nonOpaque());
 	public static void register(ModContainer mod) {
 		Registry.register(Registries.BLOCK, new Identifier(mod.metadata().id(), "fractionating_column"), FRACTIONATING_COLUMN);
 		Registry.register(Registries.BLOCK, new Identifier(mod.metadata().id(), "horizontal_pipe"), HORIZONTAL_PIPE);
