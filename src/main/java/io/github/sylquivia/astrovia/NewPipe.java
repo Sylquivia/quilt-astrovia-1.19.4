233615bb292f43c133b27b1434814d1ddcb5d0a3
Sylquivia <sylquivia@gmail.com>
2024-04-02 02:00:36 +0100

    fixed waterlogged, need to work out how to update pipes when another pipe is placed next to it (newpipe) and also how to make the hitbox of the newpipe float and not touch the floor

--- src/main/java/io/github/sylquivia/astrovia/NewPipe.java
+++ src/main/java/io/github/sylquivia/astrovia/NewPipe.java
@@ -3,6 +3,7 @@ package io.github.sylquivia.astrovia;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalConnectingBlock;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
@@ -32,17 +33,18 @@ public class NewPipe extends HorizontalConnectingBlock {
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
 		BlockView blockView = ctx.getWorld();
 		BlockPos blockPos = ctx.getBlockPos();
-		BlockState blockState = blockView.getBlockState(blockPos);
-		BlockState blockState1 = blockView.getBlockState(blockPos.north());
+		FluidState fluidState = ctx.getWorld().getFluidState(blockPos);
+		BlockState blockState = blockView.getBlockState(blockPos.north());
 		BlockState blockState2 = blockView.getBlockState(blockPos.east());
 		BlockState blockState3 = blockView.getBlockState(blockPos.south());
 		BlockState blockState4 = blockView.getBlockState(blockPos.west());
 
 		return super.getPlacementState(ctx)
-			.with(NORTH, canConnect(blockState1))
+			.with(NORTH, canConnect(blockState))
 			.with(EAST, canConnect(blockState2))
 			.with(SOUTH, canConnect(blockState3))
-			.with(WEST, canConnect(blockState4));
+			.with(WEST, canConnect(blockState4))
+			.with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
 	}
 
 	@Override
